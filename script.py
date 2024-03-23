fb5836201cb6e1f7f6cb3dc58f5e89125f79e09d
Mahdjoubabderraouf <95053965+Mahdjoubabderraouf@users.noreply.github.com>
2024-03-23 01:34:26 +0100

    Update script.py

--- script.py
+++ script.py
@@ -25,7 +25,37 @@ thread = client.beta.threads.create(
     messages=[
         {
             "role": "user",
-            "content": content,
+            "content": """
+            # importing the cv2 library
+import cv2
+
+# loading the haar case algorithm file into alg variable
+alg = "/content/haarcascade_frontalface_default.xml"
+# passing the algorithm to OpenCV
+haar_cascade = cv2.CascadeClassifier(alg)
+# loading the image path into file_name variable - replace <INSERT YOUR IMAGE NAME HERE> with the path to your image
+file_name = "/content/363563581_389519590091103_5883128900995827442_n.jpg"
+# reading the image
+img = cv2.imread(file_name, 0)
+# creating a black and white version of the image
+gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+# detecting the faces
+faces = haar_cascade.detectMultiScale(
+    gray_img, scaleFactor=1.05, minNeighbors=3, minSize=(100, 100)
+)
+
+i = 0
+# for each face detected
+for x, y, w, h in faces:
+    # crop the image to select only the face
+    cropped_image = img[y : y + h, x : x + w]
+    # loading the target image path into target_file_name variable  - replace <INSERT YOUR TARGET IMAGE NAME HERE> with the path to your target image
+    target_file_name = 'stored-faces/' + str(i) + '.jpg'
+    cv2.imwrite(
+        target_file_name,
+        cropped_image,
+    )
+    i = i + 1;""",
         }
     ]
 )
